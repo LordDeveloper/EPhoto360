7b94630f29afab951045beaf34a89f77f9ceb0d4
LordDeveloper <losrkzpp@hi2.in>
2022-03-09 01:58:14 +0330

    Published

--- examples/write_filterable_effect_text.php
+++ examples/write_filterable_effect_text.php
@@ -8,6 +8,6 @@ require_once __DIR__ .'/../vendor/autoload.php';
 print json_encode(EPhoto360::create(680, [
     'text' => ['JupiterAPI'],
     'radio0' => [
-        'radio' => '2d6f1ad1-39df-4175-9faa-9348b6ba1551', // Call EPhoto360::getEffect() method to see the list of effects.
+        'radio' => '2d6f1ad1-39df-4175-9faa-9348b6ba1551', // Call EPhoto360::getEffect() method to see the list of filter.
     ]
 ]));
